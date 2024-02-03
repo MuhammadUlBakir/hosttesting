afe39df5bed706d6212a8fecee01a0d9f730ef6c
Muhammad-ul-bakir <bakirmuhammad74@gmail.com>
2024-02-03 14:40:20 +0500

    adding

--- api/Router/Router.js
+++ api/Router/Router.js
@@ -2,13 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const path = require("path");
-
+const fs = require('fs');
 const router = express.Router();
 
 router.use(cors());
 router.use(morgan("dev"));
 router.use(express.json());
 router.use(express.urlencoded({ extended: false }));
+router.use('/images', express.static(path.join(__dirname, '../images')));
 
 router.get("/hello", (req, res) => {
   res.send("Hello");
@@ -20,8 +21,19 @@ router.get("/getdata", (req, res) => {
   });
 });
 
-// router.get("/getimages", (req, res) => {
+// router.get('/api/images/:imageName', (req, res) => {
+//   const imageName = req.params.imageName;
+//   const imagePath = path.join(__dirname, `../images`, imageName);
 
-// })
+//   fs.readFile(imagePath, (err, data) => {
+//     if (err) {
+//       console.error(err);
+//       res.status(404).send('Image not found');
+//     } else {
+//       res.writeHead(200, { 'Content-Type': 'image/jpeg' }); // Adjust the content type based on your image type
+//       res.end(data);
+//     }
+//   });
+// });
 
 module.exports = router;
