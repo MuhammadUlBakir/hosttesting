75738a3013bd500eb8a876e77cde0dc8db605de3
Muhammad-ul-bakir <bakirmuhammad74@gmail.com>
2024-02-03 12:13:14 +0500

    ready to deploy

---
+++ api/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const app = express();
+const Port = process.env.PORT || 8000;
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/api/hello", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(Port, () => console.log("server listening on port 8000"));
